19e32758d8934659b23861b1ee9cf8862e24438a
jlknhvgcfzxrstaedw476eftvyf <106548307+nihalnihalnihal@users.noreply.github.com>
2022-08-10 12:40:21 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var first_name = "ranbir";
+var last_name = "something";
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var a = "274656";
+var b = "696969";
+var x = a + b;
+console.log(x);
